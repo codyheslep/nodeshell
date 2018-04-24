57de847ff6938b76bee5d750f5d6873262396b96
Drevets <drevets@gmail.com>
2018-04-24 11:51:50 -0500

    finished cat function meow

--- bash.js
+++ bash.js
@@ -1,15 +1,19 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat')
 
 process.stdout.write('prompt >');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
+  const splitCommand = cmd.split(' ');
   if (cmd === "pwd") {
     pwd();
   }
   if (cmd === "ls") {
     ls();
+  } if (splitCommand[0] === 'cat') {
+    cat(splitCommand[1]);
   }
   process.stdout.write('\nprompt > ');
 
